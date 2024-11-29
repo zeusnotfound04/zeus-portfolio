149d172df74abb08ef0952a998f44fb6d637130d
zeusnotfound04 <zeusnotfound0@gmail.com>
2024-11-30 04:09:55 +0530

    added my info in the user data file & config the colors too

---
+++ data/user-data.js
@@ -0,0 +1,11 @@
+export const userData = {
+    githubUser: 'zeusnotfound04',
+    devUsername: "zeusnotfound",
+    github: 'https://github.com/zeusnotfound04',
+    linkedIn: 'https://www.linkedin.com/in/vishesh-prajapati-520599302/',
+    twitter: 'https://x.com/Zeus_Notfound',
+    discord: 'https://discord.gg/2dGyNmUPwa',
+    resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+    skills: ['JavaScript', 'TypeScript', 'React', 'Node.js', 'Express.js', 'Python', 'MongoDB', 'Tailwind CSS', 'Docker', 'Bootstrap'],
+    timezone: '+6'
+  };
